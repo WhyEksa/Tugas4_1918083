095a8cc95a08f2417f6e0ea7e23a1f367b24e3b2
WhyEksa <wahyu.eka.saputra186@gmail.com>
2022-10-30 19:30:12 +0700

    Navigation dan Action Bar

---
+++ app/src/main/java/com/wes/project_1918084_tokobaju/ProductShopFragment.java
@@ -0,0 +1,22 @@
+package com.wes.project_1918084_tokobaju;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ProductShopFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle
+                                     savedInstanceState) {
+        return
+                inflater.inflate(R.layout.fragment_product_shop,container,false);
+    }
+}
+
